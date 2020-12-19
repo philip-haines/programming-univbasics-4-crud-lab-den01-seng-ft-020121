3ff3b218d1a2cdbbe9c45f540172a5c19d816e55
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 20:44:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ def remove_element_from_end_of_array(array)
   animals.pop
 end
 
-remove_element_from_end_of_array(array)
+remove_element_from_end_of_array(animals)
 
 def remove_element_from_start_of_array(array)
   
