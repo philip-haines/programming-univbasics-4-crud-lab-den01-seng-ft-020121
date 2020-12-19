38bb9302ad33ed2ec9040fcf461585772ec46b91
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 20:42:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+ animals = []
 end
 
 def create_an_array
-  
+  animals = ["dog", "cat", "bird", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  animals = ["dog", "cat", "bird", "fish"]
+  animals << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  animals = ["dog", "cat", "bird", "fish"]
+  animals.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  animals = ["dog", "cat", "bird", "fish"]
+  animals.pop("fish")
 end
 
 def remove_element_from_start_of_array(array)
