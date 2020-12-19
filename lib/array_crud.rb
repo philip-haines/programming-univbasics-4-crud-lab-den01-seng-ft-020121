f98d9eec988b13c16d7e894aca6651faf83ca50e
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 20:47:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,6 +30,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array[2]
   
 end
 
