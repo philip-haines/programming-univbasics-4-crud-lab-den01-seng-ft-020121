1667957837aba56fbcc24962491a0bfe08bd6e33
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 20:51:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,7 +5,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array = ["wow", "I", "am", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
